efedc58a32aae93ae4251851f391665e2be805c9
student <s528803@ch112011m.nwmissouri.edu>
2017-11-14 14:30:42 -0600

    Initial Commit

---
+++ Drawing/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Drawing
+//
+//  Created by Kalvakota,Vaishnavi on 11/14/17.
+//  Copyright © 2017 Kalvakota,Vaishnavi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
